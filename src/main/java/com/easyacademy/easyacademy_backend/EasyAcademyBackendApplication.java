41b7942dd78afdae4c63cc4940d8dce9da4a7db8
Alessandro Vivone <v.alessandro.giovanni@gmail.com>
2025-11-17 11:21:49 +0100

    Initial commit from Spring Initializr

---
+++ src/main/java/com/easyacademy/easyacademy_backend/EasyAcademyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easyacademy.easyacademy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAcademyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyAcademyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyacademy/easyacademy_backend/EasyAcademyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyacademy.easyacademy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyAcademyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
